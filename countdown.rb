cafb8fc3141813b06864d4ce0f3c438b2e51e0e7
Ibiene LongJohn <bgirlapollo@gmail.com>
2020-05-11 04:44:52 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -1,10 +1,8 @@
 #write your code here
-require "pry"
 def countdown
   countdown_to_midnight = 10
   while countdown_to_midnight <= 0
     countdown_to_midnight += 1
     "#{countdown_to_midnight} SECOND(S)!"
-    binding.pry
   end
 end
