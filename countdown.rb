8772e68439751153530dcf79167529a8a211eaee
Ibiene LongJohn <bgirlapollo@gmail.com>
2020-05-11 03:15:43 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,9 @@
 #write your code here
 
 def countdown
+  countdown_to_midnight = 10
+  while countdown_to_midnight
+    countdown_to_midnight -= 1
+    puts "#{countdown_to_midnight} SECOND(s)"
+  end
 end
