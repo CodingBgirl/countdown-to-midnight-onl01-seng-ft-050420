ea3ca72f984d4bd9760bd8dfb2ae22984d20b142
Ibiene LongJohn <bgirlapollo@gmail.com>
2020-05-11 03:26:35 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -1,7 +1,7 @@
 #write your code here
 
 def countdown
-  countdown_to_midnight < 10
+  countdown_to_midnight <= 10
   while countdown_to_midnight
     countdown_to_midnight -= 1
     puts "#{countdown_to_midnight} SECOND(s)"
